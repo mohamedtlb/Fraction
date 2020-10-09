6e42f2552284596903e8bfe42d2a2df5fba5d726
TOLBA MOHAMED <mohamed231995@hotmail.fr>
2020-10-09 13:35:14 +0100

    mettre a jours les deux fichiers java

---
+++ Fraction.java
@@ -0,0 +1,12 @@
+
+public class Fraction {
+
+	@Override
+	public String toString() {
+		return "Fraction []";
+	}
+	
+	public Fraction(){
+		
+	}
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Fraction f = new Fraction();
+    System.out.print("bonjour");
+	}
+
+}
